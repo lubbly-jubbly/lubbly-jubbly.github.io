b69a99ed3c84786e5c1dd884e19c1d7236163d66
lubbly-jubbly <libby.rear@gmail.com>
2023-12-27 22:24:50 +0000

    change img to Image, text change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 function HomePage() {
   return (
     <main>
       <div>
-        Hello! I am a software developer, currently based in Glasgow, Scotland,
-        but hoping to move to Berlin...with your help!
+        Hello! I am a software developer, currently based in Glasgow, Scotland.
       </div>
       <div>
-        <img src="/images/fun-libby.png" alt="me" width="64" height="64" />
+        <Image src="/images/fun-libby.png" alt="me" width="64" height="64" />
       </div>
     </main>
   )
